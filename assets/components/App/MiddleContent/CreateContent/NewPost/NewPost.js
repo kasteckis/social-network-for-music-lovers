d874f703b5fc4957390da76024e37c3cf220d265
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2021-03-19 22:35:48 +0200

    Static form to create a post

--- assets/components/App/MiddleContent/CreateContent/NewPost/NewPost.js
+++ assets/components/App/MiddleContent/CreateContent/NewPost/NewPost.js
@@ -1,12 +1,85 @@
 import React, {Component} from 'react';
-import {Card, CardContent} from "@material-ui/core";
+import {
+    Button,
+    Card,
+    CardContent,
+    CircularProgress,
+    Grid,
+    TextareaAutosize,
+    TextField,
+    Typography
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
 
 class NewPost extends Component {
     render() {
         return (
             <Card className="mt-2">
                 <CardContent>
-                    naujo posto kurimo forma todo
+                    <Typography color="textPrimary" gutterBottom align="center">
+                        Naujo įrašo kūrimas
+                    </Typography>
+                    <Grid
+                        container
+                        justify="center"
+                    >
+                        <form style={{width: '100%'}} onSubmit={(event) => this.loginHandler(event)}>
+                            {/*{*/}
+                            {/*    this.props.error !== null ?*/}
+                            {/*        <Alert severity="error">{this.props.error}</Alert>*/}
+                            {/*        :*/}
+                            {/*        null*/}
+                            {/*}*/}
+                            <TextField
+                                // error={this.state.emailError}
+                                fullWidth
+                                className="mt-2"
+                                required
+                                // inputRef={this.emailRef}
+                                label="Pavadinimas"
+                                variant="outlined"
+                            /><br/>
+                            <TextField
+                                // error={this.state.emailError}
+                                fullWidth
+                                className="mt-2"
+                                // inputRef={this.emailRef}
+                                label="Spotify dainos nuoroda"
+                                variant="outlined"
+                            /><br/>
+                            <TextField
+                                placeholder="Turinys"
+                                className="mt-2"
+                                fullWidth
+                                multiline
+                                rows={10}
+                                variant="outlined"
+                                rowsMax={Infinity}
+                            /><br/>
+                            {/*<Grid*/}
+                            {/*    hidden={!this.props.loading}*/}
+                            {/*    container*/}
+                            {/*    justify="center"*/}
+                            {/*>*/}
+                            {/*    <CircularProgress className="mb-2" />*/}
+                            {/*</Grid>*/}
+
+                            <Grid
+                                container
+                                justify="center"
+                                className="mt-2"
+                            >
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={(event) => console.log("kurti")}
+                                >
+                                    Sukurti
+                                </Button>
+                            </Grid>
+                        </form>
+                    </Grid>
                 </CardContent>
             </Card>
         );
