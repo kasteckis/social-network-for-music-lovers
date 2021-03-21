57d87dff669e5cf2c9f4f1a04fbd57c1ee165ddf
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2021-03-21 17:30:03 +0200

    Fixed survey UI

--- assets/components/RightContent/Survey/Survey.js
+++ assets/components/RightContent/Survey/Survey.js
@@ -11,53 +11,64 @@ import {
     Typography
 } from "@material-ui/core";
 
-function Survey() {
-    return (
-        <Card className="mt-2" variant="outlined">
-            <CardContent>
-                <CssBaseline />
+class Survey extends React.Component {
 
-                <div>
-                    <Typography color="textPrimary" gutterBottom>
-                        Kaip sekasi?
-                    </Typography>
-                    <form noValidate>
-                        <List>
-                            <ListItem
-                                role={undefined}
-                                button
-                                // onClick={this.handleToggle(value)}
-                                // className={classes.listItem}
-                            >
-                                <FormControlLabel
-                                    control={<Radio />}
-                                    // checked={this.state.checked === value}
-                                    tabIndex={-1}
-                                    label="Gerai"
-                                />
-                            </ListItem>
-                            <ListItem
-                                role={undefined}
-                                button
-                                // onClick={this.handleToggle(value)}
-                                // className={classes.listItem}
-                            >
-                                <FormControlLabel
-                                    control={<Radio />}
-                                    // checked={this.state.checked === value}
-                                    tabIndex={-1}
-                                    label="Puikiai"
-                                />
-                            </ListItem>
-                        </List>
-                        <Button variant="contained" color="primary">
-                            Balsuoti
-                        </Button>
-                    </form>
-                </div>
-            </CardContent>
-        </Card>
-    );
+    state = {
+        checked: -1
+    }
+
+    handleToggle(value) {
+        this.setState({checked: value});
+    }
+
+    render() {
+        return (
+            <Card className="mt-2" variant="outlined">
+                <CardContent>
+                    <CssBaseline/>
+
+                    <div>
+                        <Typography color="textPrimary" gutterBottom>
+                            Kaip sekasi?
+                        </Typography>
+                        <form noValidate>
+                            <List>
+                                <ListItem
+                                    role={undefined}
+                                    button
+                                    onClick={() => this.handleToggle(0)}
+                                    // className={classes.listItem}
+                                >
+                                    <FormControlLabel
+                                        control={<Radio/>}
+                                        checked={this.state.checked === 0}
+                                        tabIndex={-1}
+                                        label="Gerai"
+                                    />
+                                </ListItem>
+                                <ListItem
+                                    role={undefined}
+                                    button
+                                    onClick={() => this.handleToggle(1)}
+                                    // className={classes.listItem}
+                                >
+                                    <FormControlLabel
+                                        control={<Radio/>}
+                                        checked={this.state.checked === 1}
+                                        tabIndex={-1}
+                                        label="Puikiai"
+                                    />
+                                </ListItem>
+                            </List>
+                            <Button variant="contained" color="primary">
+                                Balsuoti
+                            </Button>
+                        </form>
+                    </div>
+                </CardContent>
+            </Card>
+        );
+    }
 }
 
 export default Survey;
