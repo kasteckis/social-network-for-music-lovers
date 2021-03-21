daf5aa58b794322d6a4fd4b8d3e9b21cdebcbc36
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2021-03-21 20:56:25 +0200

    Changed mobile navbar to white color

--- assets/components/App/MobileNavbar/MobileNavbar.js
+++ assets/components/App/MobileNavbar/MobileNavbar.js
@@ -34,29 +34,41 @@ function MobileNavbar(props) {
     const useStyles = makeStyles({
         root: {
             backgroundColor: '#2882CE',
+            actionItemStyles: {
+                "&$selected": {
+                    color: "red"
+                }
+            },
+        },
+        label: {
+            color: 'white'
         }
     });
 
     const classes = useStyles();
 
+    const white = {
+        color: 'white'
+    }
+
     return (
         <React.Fragment>
                 {props.auth.token === null ?
                     <BottomNavigation value={value} onChange={handleChange} classes={{root: classes.root}} className="mobile-navbar" showLabels>
-                        <BottomNavigationAction label="Srautas" value="" icon={<RssFeed />} />
-                        <BottomNavigationAction label="Paieška" value="paieska" icon={<Search />} />
-                        <BottomNavigationAction label="Registruotis" value="registruotis" icon={<ExitToApp />} />
-                        <BottomNavigationAction label="Prisijungti" value="prisijungti" icon={<ExitToApp />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Srautas" value="" icon={<RssFeed style={white} />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Paieška" value="paieska" icon={<Search style={white} />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Registruotis" value="registruotis" icon={<ExitToApp style={white} />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Prisijungti" value="prisijungti" icon={<ExitToApp style={white} />} />
 
                     </BottomNavigation>
                     :
                     <BottomNavigation value={value} onChange={handleChange} classes={{root: classes.root}} className="mobile-navbar" showLabels>
                         {dialogOpen ? <CreateContentPopup open={dialogOpen} close={closeCreateContentDialogHandler} /> : null}
-                        <BottomNavigationAction label="Srautas" value="" icon={<RssFeed />} />
-                        <BottomNavigationAction label="Paieška" value="paieska" icon={<Search />} />
-                        <BottomNavigationAction label="Įkelti" value="ikelti" icon={<AddCircle />} />
-                        <BottomNavigationAction label="Draugai" value="draugai" icon={<Group />} />
-                        <BottomNavigationAction label="Profilis" value="profilis" icon={<Person />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Srautas" value="" icon={<RssFeed style={white} />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Paieška" value="paieska" icon={<Search style={white} />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Įkelti" value="ikelti" icon={<AddCircle style={white} />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Draugai" value="draugai" icon={<Group style={white} />} />
+                        <BottomNavigationAction classes={{label: classes.label}} label="Profilis" value="profilis" icon={<Person style={white} />} />
                     </BottomNavigation>
                 }
         </React.Fragment>
