a917dbaeb18da24180fc49680a24874dec90b0d6
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2021-03-28 19:25:59 +0300

    remove redundant import

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -5,7 +5,6 @@ namespace App\Repository;
 use App\Entity\Event;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use function Doctrine\ORM\QueryBuilder;
 
 /**
  * @method Event|null find($id, $lockMode = null, $lockVersion = null)
