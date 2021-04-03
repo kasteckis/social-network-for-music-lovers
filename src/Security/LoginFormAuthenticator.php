e3fe82cd805ee856a41ee5c451f014ca98f9097b
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2021-04-03 17:18:32 +0300

    Fix after redirect after logging in to admin dashboard

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -92,9 +92,9 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-            return new RedirectResponse($targetPath);
-        }
+//        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+//            return new RedirectResponse($targetPath);
+//        }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         return new RedirectResponse($this->urlGenerator->generate('admin'));
