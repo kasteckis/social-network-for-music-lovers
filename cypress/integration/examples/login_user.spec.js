a23bc0f396655d76bdac886df5d43610887bea7c
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2021-04-23 17:48:13 +0300

    Test login cypress

---
+++ cypress/integration/examples/login_user.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+context('Location', () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env('CYPRESS_BASE_URL') + '/')
+    })
+
+    it('Login user to the system', () => {
+        cy.visit(Cypress.env('CYPRESS_BASE_URL') + '/prisijungti')
+
+        cy.get('input[type=text]').first().type('cypress@cypress.dev')
+        cy.get('input[type=password]').type('cypress')
+        cy.get('button[type=submit]').click()
+
+        cy.url().should('eq', Cypress.env('CYPRESS_BASE_URL') + '/')
+    })
+
+    it('Fail login to the system', () => {
+        cy.visit(Cypress.env('CYPRESS_BASE_URL') + '/prisijungti')
+
+        cy.get('input[type=text]').first().type('notexistant@user.dev')
+        cy.get('input[type=password]').type('passwordwhichdoesntexist')
+        cy.get('button[type=submit]').click()
+
+        cy.contains('Neteisingi prisijungimo duomenys!');
+    })
+})
